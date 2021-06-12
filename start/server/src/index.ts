22fe5066cb2356156b17707714f08f0ca47ab171
A-Siam <abdullah.khaled.kamal0@gmail.com>
2021-06-12 21:22:41 +0200

    create server

--- start/server/src/index.ts
+++ start/server/src/index.ts
@@ -1,1 +1,12 @@
 import { ApolloServer } from "apollo-server";
+
+import typeDefs from "./schema";
+
+const server = new ApolloServer({
+  typeDefs,
+});
+
+server.listen(5656, () => {
+  console.log("-------[SERVER STARTED]-------");
+  console.log("Listening on:\t5656");
+});
